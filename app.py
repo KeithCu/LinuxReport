80a9122bae8ac7370bafb70d3787e4cb0d2ad15e
Keith Curtis <keithcu@gmail.com>
2020-07-10 15:22:54 -0400

    new CR headline

--- app.py
+++ app.py
@@ -234,8 +234,8 @@ else:
 
      '<a target="_blank" href = "https://youtu.be/3ywj-PZTt4g?t=355">'
      '<code><font size="9"><b>Quercetin, a Plant Pigment Supplement + Zinc Stop COVID-19</b></font></code></a>'
-     '<br/><hr/><a target="_blank" href = "https://www.youtube.com/watch?v=W9YFXo84lCk">'
-     '<code><font size="9"><b>Known About Since April</b></font></code></a>'
+     '<br/><hr/><a target="_blank" href = "https://twitter.com/__ice9/status/1281013400752009216">'
+     '<code><font size="9"><b>Quercetin Phytosome, Liposomal Quercetin, Isoquercetin, or EMIQ (Modified Isoquercetin) Needed for High Plasma Levels</b></font></code></a>'
      #'<br/><hr/><a target="_blank" href = "https://www.reporter.am/coronavirus-chinese-expert-says-herd-immunity-impossible/">'
      #'<code><font size="9"><b>Leading Shanghai Doctor - Herd Immunity Impossible!</b></font></code></a>'
 
