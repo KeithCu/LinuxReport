c2e1d71f746b31eaebd68a4011aeaea6b4d62427
= <=>
2025-04-14 23:20:27 -0400

    remove old, unused globals

--- app.py
+++ app.py
@@ -30,20 +30,6 @@ URLS_COOKIE_VERSION = "2"
 
 USE_TOR = True
 
-# Configuration variables are now set dynamically
-ALL_URLS = shared.ALL_URLS
-site_urls = shared.site_urls
-USER_AGENT = shared.USER_AGENT
-URL_IMAGES = shared.URL_IMAGES
-FAVICON = shared.FAVICON
-LOGO_URL = shared.LOGO_URL
-WEB_DESCRIPTION = shared.WEB_DESCRIPTION
-WEB_TITLE = shared.WEB_TITLE
-ABOVE_HTML_FILE = shared.ABOVE_HTML_FILE
-WELCOME_HTML = shared.WELCOME_HTML
-STANDARD_ORDER_STR = shared.STANDARD_ORDER_STR
-
 # Import routes and pass the app instance
 import routes
 routes.init_app(g_app)
-
