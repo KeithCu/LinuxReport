17d1c6a6abd4b827e63fcfb907802dcf326d6249
= <=>
2025-08-10 15:21:57 -0400

    Suppress tqdm progress bars in auto_update.py by setting the TQDM_DISABLE environment variable to '1'.

--- auto_update.py
+++ auto_update.py
@@ -66,6 +66,10 @@ logging.getLogger("httpcore").setLevel(logging.WARNING)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("openai").setLevel(logging.WARNING)
 
+# Suppress tqdm progress bars
+import os
+os.environ['TQDM_DISABLE'] = '1'
+
 logger = logging.getLogger(__name__)
 
 # Log startup information
